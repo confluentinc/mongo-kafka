d6c923a7a9c76b38e322f26048b6bc4aed795ae8
ypmahajan <ymahajan@confluent.io>
2022-06-28 15:44:26 +0530

    Testing with Default jdk8

--- build.gradle.kts
+++ build.gradle.kts
@@ -108,7 +108,7 @@ tasks.withType<JavaCompile> {
     options.release.set(8)
 }
 
-val defaultJdkVersion = 17
+val defaultJdkVersion = 8
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(defaultJdkVersion))
