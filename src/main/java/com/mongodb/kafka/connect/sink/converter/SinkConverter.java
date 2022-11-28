b5f3b8940bc0ff6e8f21b446f30650b1e3c6e2d4
Tarun Jain <107803496+tarunjain-confluent@users.noreply.github.com>
2022-11-28 09:19:42 +0530

    removed logging complete record or value (#27)

--- src/main/java/com/mongodb/kafka/connect/sink/converter/SinkConverter.java
+++ src/main/java/com/mongodb/kafka/connect/sink/converter/SinkConverter.java
@@ -39,8 +39,8 @@ public class SinkConverter {
   private static final RecordConverter BYTE_ARRAY_RECORD_CONVERTER = new ByteArrayRecordConverter();
 
   public SinkDocument convert(final SinkRecord record) {
-    LOGGER.debug(record.toString());
-
+    LOGGER.debug("record in topic:{} at partition:{}, offset:{}",
+        record.topic(), record.kafkaPartition(), record.kafkaOffset());
     BsonDocument keyDoc = null;
     if (record.key() != null) {
       keyDoc =
