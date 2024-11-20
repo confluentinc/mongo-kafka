cffa49f38318101ce72eb10fd7bf6cbd956b84d7
Rohit <32812417+rohits64@users.noreply.github.com>
2024-11-20 15:05:43 +0530

    updated release version (#62)

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ plugins {
 }
 
 group = "org.mongodb.kafka"
-version = "1.11.1-rc-6a63af3"
+version = "1.11.1-rc-699b815"
 description = "The official MongoDB Apache Kafka Connect Connector."
 
 repositories {
