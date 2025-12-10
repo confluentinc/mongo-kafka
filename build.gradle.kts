6f7a880e04a2b5b93c2f2165a204b6dacd8fe6a6
Aniket Shrimal <ashrimal@confluent.io>
2025-12-10 12:29:37 +0530

    Minor: pin avro version to 1.11.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ repositories {
 extra.apply {
     set("mongodbDriverVersion", "[4.7,4.7.99]")
     set("kafkaVersion", "2.6.0")
-    set("avroVersion", "1.9.2") // Upgrade dep to 1.12.x as part of 2.0.x connector version bump
+    set("avroVersion", "1.11.4") // Upgrade dep to 1.12.x as part of 2.0.x connector version bump
     set("connectUtilsVersion", "1.1.0")
 
     // Testing dependencies
