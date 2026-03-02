33f22361666ca6a539d6af7e54f40dcf9cebd69d
sanskargarg <sagarg@confluent.io>
2026-03-02 13:06:34 +0530

    Skip spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ plugins {
     signing
     checkstyle
     id("com.github.gmazzo.buildconfig") version "3.0.3"
-    id("com.github.spotbugs") version "4.8.0"
+    //id("com.github.spotbugs") version "4.8.0"
     id("com.diffplug.spotless") version "5.17.1"
     id("com.github.johnrengelman.shadow") version "6.1.0"
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
